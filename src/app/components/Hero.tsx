4e47de02544ec11316cd35805fd338467e47128a
Johan Lopez <johanpppp222345@gmail.com>
2023-12-29 14:50:04 -0500

    grid touch

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,21 +4,22 @@ const Hero = () => {
   return (
     <>
       <div
-        className="hero min-h-screen z-[1] min-w-screen  py-0 my-0"
+        className="hero min-h-screen z-[1] min-w-screen  py-0 my-0 brightness-90	"
         style={{
+
           backgroundImage:
             "url(https://st3.idealista.com/news/archivos/styles/fullwidth_xl/public/2016-01/100x100_prefab_01.jpg?VersionId=VgFgdH9RaNMSpVeURWjI_99.EVkLUDre&itok=F2QL2XJZ)",
         }}
       >
-        <div className="hero-overlay  backdrop-blur-[2px]    "></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-5xl">
+        <div className="hero-overlay  "></div>
+        <div className="hero-content text-center text-neutral-content opacity-100 ">
+          <div className="max-w-5xl 			">
             <h1 className="mb-5 text-5xl font-extrabold ">
               <span className="text-2xl text-rose-400 font-medium">
                 SOMOS UN EQUIPO{" "}
-              </span>
-              <br /> <span className="font-light">DE PROFESIONALES EN</span>{" "}
-              <br /> <span>DISEÑOS DE PROYECTOS</span> <br />{" "}
+              </span  >
+              <br /> <span className="font-light ">DE PROFESIONALES EN</span>{" "}
+              <br /> <span className="text-white">DISEÑOS DE PROYECTOS</span> <br />{" "}
               <span>ARQUITECTONICOS</span>{" "}
             </h1>
             <p className="mb-5 text-white font-bold"></p>
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,27 +1,29 @@
+import BlurImage from "../components/BlurImage";
+
+
 const page = () => {
   return (
     <>
       <div className="m-14 2xl:m-auto   sm:min-h-screen md:h-screen lg:h-screen  text-black  p-6  max-w-[1500px] f">
         <div className="grid   w-full h-full  gap-3  grid-cols-2 grid-rows-5  md:grid-cols-3 md:grid-rows-5  lg:grid-cols-5 lg:grid-rows-5">
           <div className="col-span-2  md:col-span-1 md:row-start-3  lg:col-span-2 lg:row-span-3   ">
-            <img
-              className="h-full w-full object-cover rounded-2xl "
-              src="https://images.unsplash.com/photo-1682687219573-3fd75f982217?q=80&w=1375&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
+            <BlurImage
+              hash="LtLi3|2h.ibp==wGnNxI.7f#R6rw"
+              src="./images/grid/premier.jpg"
             />
           </div>
           <div className=" md:row-span-2 md:col-start-4 md:row-start-4  lg:col-span-1 lg:row-span-2  lg:row-start-4   ">
-            <img
-              className="h-full w-full object-cover rounded-2xl"
-              src="https://images.unsplash.com/photo-1620336655055-088d06e36bf0?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            <BlurImage
+              hash="L8F68|A5xVxt5FxvW;M{00r9IqtR"
+              src="./images/2.jpeg"
               alt=""
+              imgClass="object-bottom"
             />
           </div>
           <div className="md:row-span-2 md:col-span-1  lg:col-span-1 lg:row-span-1 lg:row-start-4 lg:col-start-2  ">
-            <img
-              className="w-full h-full object-cover rounded-2xl"
-              src="https://images.unsplash.com/photo-1703523888420-aa4c8a2cf027?q=80&w=930&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
+            <BlurImage
+              hash="LPF=N|EOobIq?wSioIRjE,%LM|s,"
+              src="./images/1.jpeg"
             />
           </div>
           <div className="col-span-2 md:row-span-2 md:col-span-3 lg:col-span-1 lg:row-span-1 lg:row-start-5 lg:col-start-2 ">
@@ -32,11 +34,18 @@ const page = () => {
             />
           </div>
           <div className="col-start-1 row-start-1 col-span-2 row-span-2  sm:row-start-4 sm:col-span-2 sm:row-span-3  md:row-start-1 md:col-start-1 md:col-span-3 md:row-span-3 lg:col-span-3 lg:row-span-5 ">
-            <img
-              className="h-full w-full object-cover object-bottom rounded-2xl"
-              src="https://images.unsplash.com/photo-1703372619010-ed887f803eab?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
+            <section className="m-12">
+              <h1 className="text-center font-extrabold text-2xl text-white mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h1>
+              <p className="text-xl text-white leading-9">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Laudantium alias dignissimos consequatur error repellendus,
+                voluptas fugit! Officia placeat atque ratione odit similique aut
+                aliquid iure ipsam, amet ducimus, sit unde?
+              </p>
+              <i className="fa-regular fa-user"></i>
+            </section>
           </div>
         </div>
       </div>
