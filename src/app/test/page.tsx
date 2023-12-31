9543831a9556e7295609f0fa6384d771a4f97e20
Johan <johanlopezsre@gmail.com>
2023-12-30 19:04:03 -0500

    maps infoWindow

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { useEffect, useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { useState, useMemo } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 
 export default function Home() {
   const { isLoaded } = useLoadScript({
@@ -19,10 +24,61 @@ export function Map() {
     []
   );
 
- 
+  const [infoWindowOpen, setInfoWindowOpen] = useState(true); // Estado para controlar la apertura/cierre del InfoWindow
+
+  const handleMarkerClick = () => {
+    setInfoWindowOpen(!infoWindowOpen); // Invierte el estado al hacer clic en el Marker
+  };
+
+  const handleInfoWindowClose = () => {
+    setInfoWindowOpen(false); // Cierra el InfoWindow al hacer clic en el botón de cierre
+  };
+
   return (
-    <GoogleMap zoom={18} center={center} mapContainerClassName="map-container">
-      <Marker position={center} />
-    </GoogleMap>
+    <>
+      <div className="h-screen m-12">
+        <div className="flex flex-row">
+          <div className="w-1/2">
+            <GoogleMap
+              zoom={18}
+              center={center}
+              mapContainerClassName="map-container"
+            >
+              <Marker position={center} onClick={handleMarkerClick} />
+              {infoWindowOpen && ( // Renderiza el InfoWindow solo si infoWindowOpen es true
+                <InfoWindow  position={center} onCloseClick={handleInfoWindowClose}>
+                  <div className="flex flex-row justify-center max-w-xs  ">
+                    <div className="mb-2">
+                      <img
+                        className="w-24 h-24 mx-auto border-4 border-violet-400 rounded-lg"
+                        src="https://www.ebhosworks.com.ng/wp-content/uploads/2022/04/Modern-hotel-exterior-design-architecture.jpg"
+                        alt=""
+                      />
+                    </div>
+                    <div className="p-3">
+                      <h1 className="text-center text-teal-600 font-extrabold">
+                        OFICINAS DE ARQUIT MODERNA
+                      </h1>
+                      <p className="text-slate-500 font-bold">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      </p>
+                    </div>
+                  </div>
+                </InfoWindow>
+              )}
+              <div className="h-screen"></div>
+            </GoogleMap>
+          </div>
+          <div className="w-1/2 p-10">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae
+              vel, harum quasi quibusdam debitis reiciendis voluptate,
+              reprehenderit possimus cumque autem a quaerat ipsum impedit
+              doloribus numquam eveniet quia voluptatum assumenda!
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
